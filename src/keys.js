579876f17f5fcb31d0c2467a3f695c6a90b09f53
Gabriel Rodriguez <garodriguezm96@gmail.com>
2020-10-15 16:19:36 -0300

    cambios en put

---
+++ src/keys.js
@@ -0,0 +1,16 @@
+module.exports = {
+    databaseOnline: {
+        host: 'sql135.main-hosting.eu',
+        user: 'u321217442_Galmoro292612',
+        password: 'sC:Y677~',
+        database: 'u321217442_ProyectoHogar',
+
+    },
+    databaseLocal: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'halloween',
+
+    },
+}
